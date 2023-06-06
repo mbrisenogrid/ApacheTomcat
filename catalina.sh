be67b1419026c1279c7c07dbec5d66fff5b421fe
mbrisenogrid <122638102+mbrisenogrid@users.noreply.github.com>
2023-06-06 10:38:54 -0600

    Update catalina.sh

--- catalina.sh
+++ catalina.sh
@@ -1,8 +1,12 @@
 CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote"
-CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.port=9000"
-CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.rmi.port=9000"
+CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.port=9999"
+CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.rmi.port=9999"
 CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.ssl=false"
 CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.authenticate=false"
+
+CATALINA_OPTS="$CATALINA_OPTS -Djava.net.preferIPv4Stack=true"
+CATALINA_OPTS="$CATALINA_OPTS -Djava.net.preferIPv4Addresses=true"
+CATALINA_OPTS="$CATALINA_OPTS -Djava.rmi.server.hostname=3.85.193.7"
 
 CATALINA_OPTS="$CATALINA_OPTS -Xms1G"
 CATALINA_OPTS="$CATALINA_OPTS -Xmx3G"
